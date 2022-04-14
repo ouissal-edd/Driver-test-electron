7e12cfe85cb627e8c745405d994b4155c120e10b
ouissal-edd <ouissal.edouj21@gmail.com>
2022-04-14 10:12:38 +0000

    dquizz

---
+++ js/questions.js
@@ -0,0 +1,39 @@
+function questions(numero)
+{
+	var chaine="";
+	switch(numero)
+	{
+		case 1:
+		chaine="La capital des Émirats arabes unis *Abou Dabi*Paris*Venise*Dubaï*1";
+		break;
+		case 2:
+		chaine="La capital de France *Bichkek*Caracas*Paris*Lyon*3";
+		break;
+		case 3:
+		chaine="Lors de l'épreuve, quel objet vous faut-il ? *Une pièce jointe*Une pièce d'identité*Une pièce montée*Une pièce entré*4";
+		break;
+		case 4:
+		chaine="Combien de questions sont posées lors de cet examen ? *50 questions* 40 questions* 40 questions* 10 questions*1";
+		break;
+		case 5:
+		chaine="Pour être admis, il ne faut surtout pas faire plus de: * 10 erreurs* 0 erreurs* 5 erreurs* 15 erreurs*2";
+		break;
+		case 6:
+		chaine="Si il y a un petit impact sur le pare brise: *Je dois dans tout les cas changer de pare brise.*Je peux le faire réparer.*1";
+		break;
+		case 7:
+			chaine="Pour être admis, il ne faut surtout pas faire plus de: * 10 erreurs* 0 erreurs* 5 erreurs* 15 erreurs*2";
+			break;
+		case 8:
+			chaine="Combien de questions sont posées lors de cet examen ? *50 questions* 40 questions* 40 questions* 10 questions*1";
+			break;
+		case 9:
+		chaine="Combien d'angles morts existe t'il sur le véhicule? *2*5*4*0's*3";
+		break;
+		case 10:
+			chaine="Lors de l'épreuve, quel objet vous faut-il ? *Une pièce jointe*Une pièce d'identité*Une pièce montée*Une pièce entré*4";
+			break;
+	}
+
+	return chaine;
+}
---
+++ js/script.js
@@ -0,0 +1,78 @@
+var bonne_rep = 0; var nb_questions = 10;
+var nb_erreurs = 0; var mem_nb_alea="";
+var jouer=false;
+
+ nom = document.getElementById('nom').value;
+ prenom = document.getElementById('prenom').value;
+ console.log('nom',nom);
+ console.log('prenom',prenom);
+
+function debuter()
+{
+	jouer = true;
+	bonne_rep = 0; nb_questions = 10;
+	nb_erreurs = 0; mem_nb_alea="";
+	console.log('jouer',jouer);
+	init();
+	suivant();
+}
+
+function init()
+{
+	document.getElementById('restant').innerText = nb_questions;
+	var correct = document.getElementById('score').innerText = 10-nb_erreurs;
+	console.log('correct',correct);
+	if (nb_questions == 0) {
+		document.getElementById('result').innerText = "Votre scrore " + correct + " sur 10" ;
+		document.getElementById('votre-nom').innerText = "Votre nom " + nom ;
+		document.getElementById('votre-prenom').innerText = "Votre prénom " + prenom ;
+		document.getElementById("tout").classList.add('tout');
+		document.getElementById("autre").classList.remove('tout');
+
+	}
+}
+
+function valider(num_rep)
+{
+	if(jouer==false)
+	return;
+console.log('num_rep',num_rep);
+	if(num_rep != bonne_rep)
+	nb_erreurs++;
+	nb_questions--;
+	if(nb_questions==0)
+	jouer=false;
+	init();
+	suivant();
+}
+
+function suivant()
+{
+	var indice; var test=true; var nb_alea=0;
+console.log('indice',indice);
+	if(jouer==false)
+	return;
+
+	while (test==true)
+	{
+		
+		nb_alea = Math.floor(Math.random()*10) + 1;
+		if(mem_nb_alea.indexOf('-' + nb_alea + '-')>-1)
+		nb_alea = Math.floor(Math.random()*10) + 1;
+		else
+		{
+			test=false;
+			mem_nb_alea += '-' + nb_alea + '-';
+		}
+	}
+	var chaine_question = questions(nb_alea);
+	console.log('chaine_question',chaine_question);
+	var tab_question = chaine_question.split('*');
+	document.getElementById('question').innerText = tab_question[0];
+	for (indice=1; indice<=4; indice++)
+	{
+		document.getElementById('rep' + indice).innerText = tab_question[indice];
+	}
+	bonne_rep = tab_question[5];
+
+}
